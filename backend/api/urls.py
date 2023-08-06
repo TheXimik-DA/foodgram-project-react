f607ce927104157d9890ea0cd32fd47a8a6968a4
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-08-06 23:16:54 +0300

    flake8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (
+from api.views import (
     CustomUserViewSet,
     IngredientViewSet,
     RecipeViewSet,
-    TagViewSet
+    TagViewSet,
 )
 
 app_name = 'api'
